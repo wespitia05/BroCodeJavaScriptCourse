96527bfa6a298cbf358fd1787cfc8813433e7ba2
william <espiw@farmingdale.edu>
2025-11-10 20:29:28 -0500

    Bro Code js tutorial displaying alert messages

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 console.log(`hello`);
 console.log('i like pizza');
 console.log("what do you like?");
+
+// displays an alert with a message
+// window.alert(`this is an alert!`);
+// window.alert(`i like pizza!`);
