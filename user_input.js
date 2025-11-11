74ec530c1d351b55cda8ae319f422f49831f8783
william <espiw@farmingdale.edu>
2025-11-10 22:10:02 -0500

    Bro Code js user input easy way (window prompt)

--- user_input.js
+++ user_input.js
@@ -2,3 +2,10 @@
 
 // 1. easy way = window prompt
 // 2. professional way = html textbox
+
+// EASY WAY //
+let username;
+
+username = window.prompt("what's your username?");
+
+console.log(username);
