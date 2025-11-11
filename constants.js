ee843d71314e712a9fe5e8cb2bdce02a6bf34ad5
william <espiw@farmingdale.edu>
2025-11-11 14:41:06 -0500

    Bro Code js constants calculating circumference with user input

--- constants.js
+++ constants.js
@@ -1,1 +1,13 @@
 // const = a variable that can't be changed
+
+const PI = 3.14159; // const wont let you change this value
+let radius;
+let circumference;
+
+radius = window.prompt('enter the radius of a circle');
+// convert input to Number type
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+console.log(circumference);
