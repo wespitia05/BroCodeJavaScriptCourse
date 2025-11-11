27a7a744c4786775e88a98ddf685d947a6dfbac6
william <espiw@farmingdale.edu>
2025-11-10 21:21:54 -0500

    Bro Code js arithmetic operators playing with different operands

---
+++ arithmetic_operators.js
@@ -0,0 +1,22 @@
+// arithmetic operators = operands (values, variables, etc)
+//                        operators (+ - * /)
+//                        ex 11 = x + 5;
+
+let students = 30; // class size
+
+// students = students + 1; equals 31
+// students = students - 1; equals 29
+// students = students * 2; equals 60
+// students = students / 2; equals 15
+// students = students ** 2; equals 900 
+// students = students % 2; equals 0
+
+// AUGMENTED ASSIGNMENT OPERATORS //
+// students += 1; equals 31
+// students -= 1; equals 29
+// students *= 2; equals 60
+// students /= 2; equals 15
+// students **= 2; equals 900
+// students %= 2; equals 0
+
+console.log(students);
