5f0f80ade2c23031dad6ffffdbc52e5dfbf036fe
william <espiw@farmingdale.edu>
2025-11-10 20:40:50 -0500

    Bro Code js variables declaring/assigning our values and printing them in the console along with their type

--- variables.js
+++ variables.js
@@ -3,3 +3,23 @@
 
 // 1. declaration   let x;
 // 2. assignment    x = 100;
+
+// declare/assign our values
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+// print those values in the console
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+// print sentences with our values in the console
+console.log(`you are ${age} years old`);
+console.log(`the price is $${price}`);
+console.log(`your gpa is: ${gpa}/4.0`)
+
+// print the type of object each value is
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof gpa);
