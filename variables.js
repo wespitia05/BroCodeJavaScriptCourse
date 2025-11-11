b901a0b2507976b12a12209d425da3a76a9d7d5e
william <espiw@farmingdale.edu>
2025-11-10 20:47:05 -0500

    Bro Code js variables same thing but with booleans

--- variables.js
+++ variables.js
@@ -11,6 +11,9 @@ let gpa = 2.1;
 let firstName = "william"
 let favoriteFood = "pizza"
 let email = "will123@gmail.com"
+let online = true;
+let forSale = false;
+let isStudent = true;
 
 // print those values in the console
 console.log(age);
@@ -19,6 +22,9 @@ console.log(gpa);
 console.log(firstName);
 console.log(favoriteFood);
 console.log(email);
+console.log(online);
+console.log(forSale);
+console.log(isStudent);
 
 // print sentences with our values in the console
 console.log(`you are ${age} years old`);
@@ -27,6 +33,9 @@ console.log(`your gpa is: ${gpa}/4.0`);
 console.log(`my name is ${firstName}`);
 console.log(`my favorite food is ${favoriteFood}`);
 console.log(`my email is ${email}`);
+console.log(`am i online? ${online}`);
+console.log(`is this car for sale? ${forSale}`);
+console.log(`enrolled? ${isStudent}`);
 
 // print the type of object each value is
 console.log(typeof age);
@@ -35,3 +44,6 @@ console.log(typeof gpa);
 console.log(typeof firstName);
 console.log(typeof favoriteFood);
 console.log(typeof email);
+console.log(typeof online);
+console.log(typeof forSale);
+console.log(typeof isStudent);
