7b80e712d875286c85f4636655e370696ecbdd01
william <espiw@farmingdale.edu>
2025-11-10 20:44:09 -0500

    Bro Code js variables doing same thing but with string values

--- variables.js
+++ variables.js
@@ -8,18 +8,30 @@
 let age = 25;
 let price = 10.99;
 let gpa = 2.1;
+let firstName = "william"
+let favoriteFood = "pizza"
+let email = "will123@gmail.com"
 
 // print those values in the console
 console.log(age);
 console.log(price);
 console.log(gpa);
+console.log(firstName);
+console.log(favoriteFood);
+console.log(email);
 
 // print sentences with our values in the console
 console.log(`you are ${age} years old`);
 console.log(`the price is $${price}`);
-console.log(`your gpa is: ${gpa}/4.0`)
+console.log(`your gpa is: ${gpa}/4.0`);
+console.log(`my name is ${firstName}`);
+console.log(`my favorite food is ${favoriteFood}`);
+console.log(`my email is ${email}`);
 
 // print the type of object each value is
 console.log(typeof age);
 console.log(typeof price);
 console.log(typeof gpa);
+console.log(typeof firstName);
+console.log(typeof favoriteFood);
+console.log(typeof email);
