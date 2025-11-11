29329b158c2223e60d0df14c4bf14f65b00f07f7
william <espiw@farmingdale.edu>
2025-11-10 20:32:00 -0500

    Bro Code js tutorial setting text through js

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ console.log("what do you like?");
 // displays an alert with a message
 // window.alert(`this is an alert!`);
 // window.alert(`i like pizza!`);
+
+// getting element id and setting it equal to a text
+document.getElementById("myH1").textContent = `hello world!`;
+document.getElementById("myP").textContent = `i like pizza`
