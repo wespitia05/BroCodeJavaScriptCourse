809f2eea965525c39c9780d61e51d89c0cf8751a
william <espiw@farmingdale.edu>
2025-11-10 22:17:33 -0500

    Bro Code js user input professional way (html textbox)

--- user_input.js
+++ user_input.js
@@ -4,8 +4,23 @@
 // 2. professional way = html textbox
 
 // EASY WAY //
+/*
 let username;
 
 username = window.prompt("what's your username?");
 
 console.log(username);
+*/
+
+// PROFESSIONAL WAY //
+let username;
+
+// get the submit button element, when clicked it will do the following function
+document.getElementById("mySubmit").onclick = function() {
+    // sets username to equal the value of what we typed
+    username = document.getElementById("myText").value;
+    // displays welcome "user input" h1 element after pressing the submit button
+    document.getElementById("myH1").textContent = `Welcome ${username}`;
+    // print it in the console
+    console.log(username);
+}
