6552fb9827aef0bad0d5dd690515040fccdc56fc
william <espiw@farmingdale.edu>
2025-11-11 14:47:30 -0500

    Bro Code js constants calculating circumference with html elements

--- constants.js
+++ constants.js
@@ -4,10 +4,19 @@ const PI = 3.14159; // const wont let you change this value
 let radius;
 let circumference;
 
-radius = window.prompt('enter the radius of a circle');
+// radius = window.prompt('enter the radius of a circle');
 // convert input to Number type
-radius = Number(radius);
+// radius = Number(radius);
 
-circumference = 2 * PI * radius;
+// circumference = 2 * PI * radius;
 
-console.log(circumference);
+// when the submit button is pressed, do the following function
+document.getElementById("mySubmit").onclick = function() {
+    // get value inputted as our radius
+    radius = document.getElementById("myText").value;
+    // cast type our input into a number
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    // set our h3 element to display circumference value
+    document.getElementById("myH3").textContent = circumference + "cm";
+}
