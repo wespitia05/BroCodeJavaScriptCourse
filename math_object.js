39dfdb6d7e5b0f738ec00c7fda953939b6f221f2
william <espiw@farmingdale.edu>
2025-11-12 18:09:19 -0500

    Bro Code js math object different usages

--- math_object.js
+++ math_object.js
@@ -1,2 +1,27 @@
 // Math = built in object that provides a
 //        collection of properties and methods
+
+console.log(Math.PI);
+
+let x = 3.21;
+let y = 2.99;
+let z = 1;
+
+// z = Math.round(x); // rounds regularly
+// z = Math.floor(y); // rounds down
+// z = Math.ceil(x); // rounds up
+// z = Math.trunc(x); // removes anything after the decimal
+// z = Math.pow(x, y); // 3.21^2.99
+// z = Math.sqrt(x); // squareroot
+// z = Math.log(x); // logarithm
+// z = Math.sin(x); // sine
+// z = Math.cos(x); // cosine
+// z = Math.tan(x); // tangent
+// z = Math.abs(x); // absolute value
+// z = Math.sign(x);
+let max = Math.max(x, y, z); 
+let min = Math.min(x, y ,z);
+
+console.log(z);
+console.log(max);
+console.log(min);
