33f863f2d4ae76166d9c57862130a1e6a013dfee
william <espiw@farmingdale.edu>
2025-11-12 20:49:09 -0500

    Bro Code js random number generator functionality + generates 3 random numbers

--- random_number_generator.js
+++ random_number_generator.js
@@ -3,6 +3,26 @@
 // generates a random number
 // * 6 gives us a random number from 1-6
 // floor will round down to avoid any decimal numbers
-let randomNum = Math.floor(Math.random() * 6) + 1;
+// let randomNum = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNum);
+// console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function() {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
