5e775ed919abbab39f752648d5f6dcc104a976cf
william <espiw@farmingdale.edu>
2025-11-12 23:27:32 -0500

    Bro Code js if statements, basic console outputs

--- if_statements.js
+++ if_statements.js
@@ -1,2 +1,34 @@
 // if statements = if a condition is true, execute some code
 //                 if not, do something else
+
+/* 
+let age = 13;
+
+if(age >= 18) {
+    console.log("you are old enough to enter this site");
+}
+else {
+    console.log("you must be 18+ to enter this site");
+}
+*/
+
+/*
+let time = 14;
+
+if(time < 12) {
+    console.log("good morning");
+}
+else {
+    console.log("good afternoon")
+}
+*/
+
+let isStudent = true;
+
+if(isStudent) {
+    console.log("you are a student");
+}
+
+else {
+    console.log("you are not a student");
+}
