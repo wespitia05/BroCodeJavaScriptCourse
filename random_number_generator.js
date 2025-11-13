c83382df3cb584a4c2b174888fd41967e993a4b7
william <espiw@farmingdale.edu>
2025-11-12 19:24:47 -0500

    Bro Code js random number generator printing random number from 1-6 in console

--- random_number_generator.js
+++ random_number_generator.js
@@ -1,1 +1,8 @@
 // random number generator
+
+// generates a random number
+// * 6 gives us a random number from 1-6
+// floor will round down to avoid any decimal numbers
+let randomNum = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomNum);
