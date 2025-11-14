35146176821c1d0011fd4b3b551feaf9926b6de1
william <espiw@farmingdale.edu>
2025-11-14 14:29:12 -0500

    Bro Code js ternary operator 2 examples

--- ternary_operator.js
+++ ternary_operator.js
@@ -2,3 +2,15 @@
 //                    helps to assign a variable based on a condition
 //                    condition ? codeIfTrue : codeIfFalse;
 
+/*
+let age = 17;
+let message = age >= 18 ? "youre an adult" : "youre not an adult"
+console.log(message);
+*/
+
+/*
+let time = 11;
+let greeting = time < 12 ? "good morning!" : "good afternoon!";
+console.log(greeting);
+*/
+
