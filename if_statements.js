e43b7a0fb816f50be43355722f75f62649bb3a9d
william <espiw@farmingdale.edu>
2025-11-13 23:09:37 -0500

    Bro Code js if statements html example

--- if_statements.js
+++ if_statements.js
@@ -56,6 +56,7 @@ else {
 */
 
 // ELSE IF STATEMENTS //
+/*
 let age = 18;
 
 if(age >= 100) {
@@ -72,4 +73,31 @@ else if(age < 0) {
 }
 else {
     console.log("you must be 18+ to enter this site");
+}
+*/
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function() {
+    age = myText.value;
+    age = Number(age);
+
+    if(age >= 100) {
+        resultElement.textContent = `you are too old to enter this site`;
+    }
+    else if(age == 0) {
+        resultElement.textContent = `you can't enter, you were just born`;
+    }
+    else if(age >= 18) {
+        resultElement.textContent = `you are old enough to enter this site`;
+    }
+    else if(age < 0) {
+        resultElement.textContent = `your age can't be below 0`;
+    }
+    else {
+        resultElement.textContent = `you must be 18+ to enter this site`;
+    }
 }
