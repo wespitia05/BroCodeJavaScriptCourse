3c9a0e8b4d0611cccce40a2aea0b3052383fe7dd
william <espiw@farmingdale.edu>
2025-11-14 14:25:58 -0500

    Bro Code js ternary operator setup

---
+++ ternary_operator.js
@@ -0,0 +1,4 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                    helps to assign a variable based on a condition
+//                    condition ? codeIfTrue : codeIfFalse;
+
