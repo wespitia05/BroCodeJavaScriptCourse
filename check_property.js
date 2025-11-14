9da20141a3d5ebf6adb3a2a59710159fdf7a0165
william <espiw@farmingdale.edu>
2025-11-14 14:17:53 -0500

    Bro Code js checked property functionality

--- check_property.js
+++ check_property.js
@@ -1,2 +1,32 @@
 // .checked = property that determines the checked state of an
 //            html checkbox or radio button element
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function() {
+    if(myCheckBox.checked) {
+        subResult.textContent = `you are subscribed`;
+    }
+    else {
+        subResult.textContent = `you are NOT subscribed`;
+    }
+
+    if(visaBtn.checked) {
+        paymentResult.textContent = `you are paying with visa`;
+    }
+    else if(masterCardBtn.checked) {
+        paymentResult.textContent = `you are paying with mastercard`;
+    }
+    else if(payPalBtn.checked) {
+        paymentResult.textContent = `you are paying with paypal`;
+    }
+    else {
+        paymentResult.textContent = `you must select a payment type`;
+    }
+}
