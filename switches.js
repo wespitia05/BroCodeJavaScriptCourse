3af67bb5b98d566d659b0edaf98faf191099897b
william <espiw@farmingdale.edu>
2025-11-15 18:55:41 -0500

    Bro Code js switches first example

--- switches.js
+++ switches.js
@@ -1,1 +1,30 @@
 // switch = can be an efficient replacement to many else if statements
+
+let day = "pizza";
+
+// add break after each case, otherwise we will run all code after that line
+switch(day) {
+    case 1:
+        console.log("it is monday");
+        break;
+    case 2:
+        console.log("it is tuesday");
+        break;
+    case 3:
+        console.log("it is wednesday");
+        break;
+    case 4:
+        console.log("it is thursday");
+        break;
+    case 5:
+        console.log("it is friday");
+        break;
+    case 6:
+        console.log("it is saturday");
+        break;
+    case 7:
+        console.log("it is sunday");
+        break;
+    default:
+        console.log(`${day} is not a day`);
+}
