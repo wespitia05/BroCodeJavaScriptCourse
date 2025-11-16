4fdef5c8ddf7d3141890dd38b464f765fbb56300
william <espiw@farmingdale.edu>
2025-11-15 19:02:34 -0500

    Bro Code js switches second example

--- switches.js
+++ switches.js
@@ -1,5 +1,6 @@
 // switch = can be an efficient replacement to many else if statements
 
+/*
 let day = "pizza";
 
 // add break after each case, otherwise we will run all code after that line
@@ -28,3 +29,50 @@ switch(day) {
     default:
         console.log(`${day} is not a day`);
 }
+*/
+
+let testScore = 76;
+let letterGrade;
+
+switch(true) {
+    case testScore >= 97:
+        letterGrade = "A+";
+        break;
+    case testScore >= 93:
+        letterGrade = "A";
+        break;
+    case testScore >= 90:
+        letterGrade = "A-";
+        break;
+    case testScore >= 87:
+        letterGrade = "B+";
+        break;
+    case testScore >= 83:
+        letterGrade = "B";
+        break;
+    case testScore >= 80:
+        letterGrade = "B-";
+        break;
+    case testScore >= 77:
+        letterGrade = "C+";
+        break;
+    case testScore >= 73:
+        letterGrade = "C";
+        break;
+    case testScore >= 70:
+        letterGrade = "C-";
+        break;
+    case testScore >= 67:
+        letterGrade = "D+";
+        break;
+    case testScore >= 63:
+        letterGrade = "D";
+        break;
+    case testScore >= 60:
+        letterGrade = "D-";
+        break;
+    default:
+        letterGrade = "F";
+}
+
+console.log(letterGrade);
