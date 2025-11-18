30308af01c053d1cea4cd69e50da241a48530f6c
william <espiw@farmingdale.edu>
2025-11-17 20:19:33 -0500

    Bro Code js method chaining setup

---
+++ method_chaining.js
@@ -0,0 +1,10 @@
+// method chaining = calling one method after another 
+//                   in one continuous line of code
+
+// ----- NO METHOD CHAINING -----  //
+
+
+
+
+
+// ----- METHOD CHAINING ----- //
