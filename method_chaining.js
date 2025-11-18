ec0ab5cdc535f4519b544d7f3219fd8d6cf391a4
william <espiw@farmingdale.edu>
2025-11-17 20:24:10 -0500

    Bro Code js no method chaining example

--- method_chaining.js
+++ method_chaining.js
@@ -2,9 +2,18 @@
 //                   in one continuous line of code
 
 // ----- NO METHOD CHAINING -----  //
+let username = window.prompt("enter your username:");
 
+// main idea is to make first letter uppercase,
+// and make every letter after the first letter lowercase
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
 
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
 
-
+console.log(username);
 
 // ----- METHOD CHAINING ----- //
