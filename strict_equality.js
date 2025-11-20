9ca1cc298beef193b16e55134dee28c9d8b7ddd0
william <espiw@farmingdale.edu>
2025-11-19 19:33:36 -0500

    Bro Code js strict equality setup

---
+++ strict_equality.js
@@ -0,0 +1,5 @@
+//   = assignment operator
+//  == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatypes are equal)
+//  != inequality operator
+// !== strict inequality operator
