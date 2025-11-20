3c5cd72455711212a884da6f844d34dac4c3c8de
william <espiw@farmingdale.edu>
2025-11-19 21:20:38 -0500

    Bro Code js while loop example one

--- while_loop.js
+++ while_loop.js
@@ -1,1 +1,9 @@
 // while loop = repeat some code WHILE some condition is true
+
+let username = "";
+
+while(username === "" || username ===null) {
+    username = window.prompt(`enter your name:`);
+}
+
+console.log(`hello ${username}`);
