f2d89e5ed1ce03a9525450a9e25982d6e12ac476
william <espiw@farmingdale.edu>
2025-11-19 19:39:19 -0500

    Bro Code js strict equality examples

--- strict_equality.js
+++ strict_equality.js
@@ -3,3 +3,39 @@
 // === strict equality operator (compare if values & datatypes are equal)
 //  != inequality operator
 // !== strict inequality operator
+
+const PI = 3.14;
+
+/* compares only if value are equal (true)
+if(PI == "3.14") {
+    console.log("that is pi");
+}
+else {
+    console.log("that is not pi");
+}
+*/
+
+/* compares value AND datatype (false)
+if(PI === "3.14") {
+    console.log("that is pi");
+}
+else {
+    console.log("that is not pi");
+}
+*/
+
+/* checks if values match or not (true)
+if(PI != "3.14") {
+    console.log("that is not pi");
+}
+else {
+    console.log("that is pi");
+}
+*/
+
+if(PI !== "3.14") {
+    console.log("that not pi");
+}
+else {
+    console.log("that is pi");
+}
