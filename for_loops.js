1f1cfefb40c9ed0cdfa1d08bc20a601e6fb72f45
william <espiw@farmingdale.edu>
2025-11-20 20:28:15 -0500

    Bro Code js for loops couple of examples

--- for_loops.js
+++ for_loops.js
@@ -1,1 +1,39 @@
-// for lopp = repeat some code a limited amount of times
+// for loop = repeat some code a limited amount of times
+
+// parameters (starting point, until when, increment/decrement)
+
+// prints 0-9
+/*
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+*/
+
+// prints 1-10
+/*
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+*/
+
+// prints 1, 3, 7, 9
+/*
+for(let i = 1; i < 10; i+=2) {
+    console.log(i);
+}
+*/
+
+// prints 2, 4, 6, 8, 10
+/*
+for(let i = 2; i <= 10; i+=2) {
+    console.log(i);
+}
+*/
+
+// prints 10-1
+/*
+for(let i = 10; i > 0; i--) {
+    console.log(i);
+}
+console.log("HAPPY NEW YEAR");
+*/
