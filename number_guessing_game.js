e086231dd72730e0de096f6be218cd68c6eb8746
william <espiw@farmingdale.edu>
2025-11-20 20:38:44 -0500

    Bro Code js number guessing game setup + constants

---
+++ number_guessing_game.js
@@ -0,0 +1,7 @@
+// ----- NUMBER GUESSING GAME ----- //
+
+const minNum = 1; // our minimum number
+const maxNum = 100; // our maximum number
+// generates a random number from 1-100
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
