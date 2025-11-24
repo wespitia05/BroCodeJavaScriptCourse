d85814ae01c85be7cb0af8e19a5cd5072cb14cb8
william <espiw@farmingdale.edu>
2025-11-24 17:52:04 -0500

    Bro Code js functions example one

--- functions.js
+++ functions.js
@@ -2,3 +2,15 @@
 //            declares code once, use it whenever you want
 //            calls the function to execute the code
 
+// example one
+function happyBirthday(username, age) {
+    console.log("happy birthday to you!");
+    console.log("happy birthday to you!");
+    console.log(`happy birthday dear ${username}!`);
+    console.log("happy birthday to you!");
+    console.log(`you are ${age} years old`);
+}
+
+happyBirthday("william", 25);
+happyBirthday("spongebob", 32);
+happyBirthday("patrick", 37);
