654909c3d09b604880fbbd75ac30c5e7e9ebf63f
william <espiw@farmingdale.edu>
2025-11-25 20:03:04 -0500

    Bro Code js variable scope practice

--- variable_scope.js
+++ variable_scope.js
@@ -1,2 +1,16 @@
 // variable scope = where a variable is recognized 
 //                  and accessible (local vs global)
+
+let x = 3;
+
+function1();
+
+function function1() {
+    let x = 1;
+    console.log(x);
+}
+
+function function2() {
+    let x = 2;
+    console.log(x);
+}
