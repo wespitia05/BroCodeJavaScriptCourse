fbbceb2a8613eb4627e69f0eae7b5894e51fdedd
william <espiw@farmingdale.edu>
2025-11-28 14:56:34 -0500

    Bro Code js arrays adding/removing elements

--- arrays.js
+++ arrays.js
@@ -1,1 +1,19 @@
 // array = a variable like structure that can hold more than 1 value
+
+// each item in the array known as an element
+let fruits = ["apple", "orange", "banana"];
+
+// fruits[0] = "coconut"; // changes value at index 0
+
+// adding element at the end of the array
+fruits.push("coconut");
+// removing element at the end of the array
+// fruits.pop();
+// adding element at the beginning of the array
+fruits.unshift("mango");
+// removing element at the beginning of the array
+// fruits.shift();
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
