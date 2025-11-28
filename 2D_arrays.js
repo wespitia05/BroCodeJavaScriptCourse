8c15aacbcd7bc6972f726ffcdfdaca12da270bf3
william <espiw@farmingdale.edu>
2025-11-28 15:11:16 -0500

    Bro Code js 2D arrays setup

---
+++ 2D_arrays.js
@@ -0,0 +1,7 @@
+// 2D array = multi-dimentsional array that stores a matrix
+//            of data in rows and columns
+//            useful for games, spreadsheets, or representing images
+
+//            [X, O, X]
+//            [O, X, O]
+//            [X, O, X]
