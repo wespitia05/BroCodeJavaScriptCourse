7eb52d55d643e965afb6e349078ca0c6443eb490
william <espiw@farmingdale.edu>
2025-11-28 15:28:28 -0500

    Bro Code js spread operator with numbers

--- spread_operator.js
+++ spread_operator.js
@@ -1,3 +1,11 @@
 // spread operator = ... allows an iterable such as an array or string
 //                   to be exapnded in separate elements (unpack elements)
 
+let numbers = [1, 2, 3, 4, 5];
+// use spread operator to get maximum/minimum
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
+
+console.log(numbers);
+console.log(maximum);
+console.log(minimum);
