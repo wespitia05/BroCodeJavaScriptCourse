8135baa9d8e1c107ec9dbced659cc9cb325a9619
william <espiw@farmingdale.edu>
2025-11-29 15:42:22 -0500

    Bro Code js rest parameters example three

--- rest_parameters.js
+++ rest_parameters.js
@@ -26,6 +26,7 @@ const foods = getFood(food1, food2, food3, food4, food5);
 console.groupCollapsed(foods);
 */
 
+/*
 function sum(...numbers) {
     let result = 0;
     for(let number of numbers) {
@@ -47,3 +48,12 @@ const average = getAverage(75, 100, 85, 90, 50);
 
 console.log(`your total is $${total}`);
 console.log(`your average is ${average}`);
+*/
+
+function combineStrings(...strings) {
+    return strings.join(" ");
+}
+
+const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
+
+console.log(fullName);
