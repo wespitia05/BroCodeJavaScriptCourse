f54274eca3b7b5bd685739aef958a1945310ae2d
william <espiw@farmingdale.edu>
2025-11-29 15:31:36 -0500

    Bro Code js rest parameters setup

---
+++ rest_parameters.js
@@ -0,0 +1,5 @@
+// rest parameters = (...rest) allows a function work with a variable
+//                   number of arguments by bundling them into an array
+
+//                   spread = expands an array into separate elements
+//                   rest = bundles separate elements into an array
