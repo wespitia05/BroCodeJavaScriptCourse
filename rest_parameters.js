c754eef157b918dbc51e445f3c0bb9f04ef3f5ad
william <espiw@farmingdale.edu>
2025-11-29 15:37:02 -0500

    Bro Code js rest parameters example one

--- rest_parameters.js
+++ rest_parameters.js
@@ -3,3 +3,23 @@
 
 //                   spread = expands an array into separate elements
 //                   rest = bundles separate elements into an array
+
+function openFridge(...foods) {
+    console.log(foods); // displays as an array
+    console.log(...foods); // spread operator separates them
+}
+
+function getFood(...foods) {
+    return foods;
+}
+
+const food1 = "pizza";
+const food2 = "hamburger";
+const food3 = "hot dog";
+const food4 = "sushi";
+const food5 = "ramen";
+
+// openFridge(food1, food2, food3, food4, food5)
+
+const foods = getFood(food1, food2, food3, food4, food5);
+console.groupCollapsed(foods);
