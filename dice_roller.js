c20008cc130dcbabb5fe21f76dcd62ffd62d1c56
william <espiw@farmingdale.edu>
2025-11-30 14:57:57 -0500

    Bro Code js dice roller functionality and final css touches

--- dice_roller.js
+++ dice_roller.js
@@ -1,1 +1,20 @@
 /* ----- DICE ROLLER PROGRAM ----- */
+
+function rollDice() {
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImgs = document.getElementById("diceImgs");
+    const values = []; // holds the value of the dice
+    const imgs = []; // holds the imgs based on those values
+
+    for(let i = 0; i < numOfDice; i++) {
+        // get random number between 1 and 6
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value); // add our value into the empty values array
+        imgs.push(`<img src="dice_imgs/${value}.png" alt="Dice ${value}">`);
+    }
+    // displays dice value, followed by a comma
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    // combines the images to display based on the selected values
+    diceImgs.innerHTML = imgs.join('');
+}
