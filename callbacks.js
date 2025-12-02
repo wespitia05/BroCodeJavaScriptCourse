17bd0040a027f5f237acc40d67b235a9b949c420
william <espiw@farmingdale.edu>
2025-12-01 22:54:41 -0500

    Bro Code js callbacks example one

--- callbacks.js
+++ callbacks.js
@@ -7,3 +7,24 @@
 //            3. interacting with databases
 
 //            "hey, when you're done, call this next"
+
+hello(goodbye);
+hello(leave);
+hello(wait);
+
+function hello(callback) {
+    console.log("hello!");
+    callback();
+}
+
+function leave() {
+    console.log("leave!");
+}
+
+function wait() {
+    console.log("wait!");
+}
+
+function goodbye() {
+    console.log("goodbye!");
+}
