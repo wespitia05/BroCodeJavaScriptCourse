a3502d86da05bcee7e5b219624c8b80244286f53
william <espiw@farmingdale.edu>
2025-12-01 19:22:40 -0500

    Bro Code js random password generator setting up constants and function names

--- password_generator.js
+++ password_generator.js
@@ -1,1 +1,19 @@
 // ----- RANDOM PASSWORD GENERATOR ----- //
+
+function generatePassword(lenght, lowercase, uppercase, numbers, symbols) {
+    return '';
+}
+
+const passwordLength = 12; 
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, 
+                                  includeLowercase,
+                                  includeUppercase,
+                                  includeNumbers, 
+                                  includeSymbols);
+
+console.log(`generated password: ${password}`);
