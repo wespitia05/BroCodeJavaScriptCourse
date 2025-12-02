0d68fc3425b05c2f8d0ce2ec7cdf8f7975a105db
william <espiw@farmingdale.edu>
2025-12-01 23:01:29 -0500

    Bro Code js forEach() setup

---
+++ forEach().js
@@ -0,0 +1,5 @@
+// forEach() = method used to iterate over the elements
+//             of an array and apply a specified function (callback)
+//             to each element
+
+//             array.forEach(callback)
