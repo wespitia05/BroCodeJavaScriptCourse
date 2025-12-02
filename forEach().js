91271ba20bf6589ba49e2e285b68a2572b855251
william <espiw@farmingdale.edu>
2025-12-02 18:48:25 -0500

    Bro Code js forEach() example one

--- forEach().js
+++ forEach().js
@@ -3,3 +3,31 @@
 //             to each element
 
 //             array.forEach(callback)
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(cube);
+numbers.forEach(square);
+numbers.forEach(triple);
+numbers.forEach(double);
+numbers.forEach(display);
+
+function double(element, index, array) {
+    array[index] = element * 2;
+}
+
+function triple(element, index, array) {
+    array[index] = element * 3;
+}
+
+function square(element, index, array) {
+    array[index] = Math.pow(element, 2);
+}
+
+function cube(element, index, array) {
+    array[index] = Math.pow(element, 3);
+}
+
+function display(element) {
+    console.log(element);
+}
