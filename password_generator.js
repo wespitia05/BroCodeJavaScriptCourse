f7ed85b74754aee32edd8c056795b6f4ff97b824
william <espiw@farmingdale.edu>
2025-12-01 19:30:32 -0500

    Bro Code js random password generator setting up characters we can use in the password

--- password_generator.js
+++ password_generator.js
@@ -1,6 +1,23 @@
 // ----- RANDOM PASSWORD GENERATOR ----- //
 
-function generatePassword(lenght, lowercase, uppercase, numbers, symbols) {
+function generatePassword(length, lowercase, uppercase, numbers, symbols) {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_-+=";
+
+    let allowedChars = ""; // allowed characters will be an empty string
+    let password = ""; // password will be an empty string
+
+    // concatenates allowed characters into the allowedChars string
+    // if any of the characters are true, add it to the allowedChars otherwise add nothing
+    allowedChars += lowercase ? lowercaseChars : "";
+    allowedChars += uppercase ? uppercaseChars : "";
+    allowedChars += numbers ? numberChars : "";
+    allowedChars += symbols ? symbolChars : "";
+
+    console.log(allowedChars); // checks to see if all our characters are included
+
     return '';
 }
 
