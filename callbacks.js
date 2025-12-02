5d79469b81cc487cdd9b297ae19829a5238d0e88
william <espiw@farmingdale.edu>
2025-12-01 22:50:02 -0500

    Bro Code js callbacks setup

---
+++ callbacks.js
@@ -0,0 +1,9 @@
+// callback = a function that is passed as an arrgument
+//            to another function
+
+//            used to handle asynchronous operations:
+//            1. reading a file
+//            2. network requests
+//            3. interacting with databases
+
+//            "hey, when you're done, call this next"
