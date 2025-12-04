207351cb8519db2ba5762f5eb3dd835768e470a5
william <espiw@farmingdale.edu>
2025-12-03 21:40:53 -0500

    Bro Code js map() example one

--- map().js
+++ map().js
@@ -1,2 +1,17 @@
 // .map() = accepts a callback and applies that function
 //          to each element of an array, then returns a new array
+
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+
+console.log(squares);
+console.log(cubes);
+
+function square(element) {
+    return Math.pow(element, 2);
+}
+
+function cube(element) {
+    return Math.pow(element, 3);
+}
