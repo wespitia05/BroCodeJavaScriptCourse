f5de3370c2f4b5769b4af44d38be266ffb7454f6
william <espiw@farmingdale.edu>
2025-12-03 21:43:10 -0500

    Bro Code js map() example two

--- map().js
+++ map().js
@@ -1,6 +1,7 @@
 // .map() = accepts a callback and applies that function
 //          to each element of an array, then returns a new array
 
+/*
 const numbers = [1, 2, 3, 4, 5];
 const squares = numbers.map(square);
 const cubes = numbers.map(cube);
@@ -14,4 +15,20 @@ function square(element) {
 
 function cube(element) {
     return Math.pow(element, 3);
+}
+*/
+
+const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+console.log(studentsUpper);
+console.log(studentsLower);
+
+function upperCase(element) {
+    return element.toUpperCase();
+}
+
+function lowerCase(element) {
+    return element.toLowerCase();
 }
