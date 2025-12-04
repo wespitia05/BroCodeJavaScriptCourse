7505c4ed9104f574d069674063eb84f837d0f6c9
william <espiw@farmingdale.edu>
2025-12-03 21:35:32 -0500

    Bro Code js forEach() example two

--- forEach().js
+++ forEach().js
@@ -4,6 +4,7 @@
 
 //             array.forEach(callback)
 
+/*
 let numbers = [1, 2, 3, 4, 5];
 
 numbers.forEach(cube);
@@ -26,6 +27,25 @@ function square(element, index, array) {
 
 function cube(element, index, array) {
     array[index] = Math.pow(element, 3);
+}
+
+function display(element) {
+    console.log(element);
+}
+*/
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+fruits.forEach(capitalize);
+// fruits.forEach(upperCase);
+// fruits.forEach(display);
+
+function upperCase(element, index, array) {
+    array[index] = element.toUpperCase();
+}
+
+function capitalize(element, index, array) {
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
 }
 
 function display(element) {
