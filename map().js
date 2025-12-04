cad03a72ac31aedd545173271f36c9955b853793
william <espiw@farmingdale.edu>
2025-12-03 21:46:12 -0500

    Bro Code js map() example three

--- map().js
+++ map().js
@@ -18,6 +18,7 @@ function cube(element) {
 }
 */
 
+/*
 const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
 const studentsUpper = students.map(upperCase);
 const studentsLower = students.map(lowerCase);
@@ -31,4 +32,15 @@ function upperCase(element) {
 
 function lowerCase(element) {
     return element.toLowerCase();
+}
+*/
+
+const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+const formattedDates = dates.map(formatDates);
+
+console.log(formattedDates);
+
+function formatDates(element) {
+    const parts = element.split("-");
+    return `${parts[1]}/${parts[2]}/${parts[0]}`
 }
