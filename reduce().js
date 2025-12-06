74fed40870ad0fd92f70e51e676ddc3ab829a839
william <espiw@farmingdale.edu>
2025-12-06 13:39:22 -0500

    Bro Code js reduce() example one

--- reduce().js
+++ reduce().js
@@ -1,1 +1,13 @@
 // .reduce() = reduce elements of an array to a single value
+
+const prices = [5, 30, 10, 25, 15, 20];
+
+const total = prices.reduce(sum);
+
+console.log(`$${total.toFixed(2)}`);
+
+// 5 + 30 = 35
+// 35 + 10 = 45, etc etc
+function sum(previous, next) {
+    return previous + next;
+}
