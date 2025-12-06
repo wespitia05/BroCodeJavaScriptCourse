9d1db1c6c3cff998fd63844224f44fd772ed9389
william <espiw@farmingdale.edu>
2025-12-06 13:42:08 -0500

    Bro Code js reduce() example two

--- reduce().js
+++ reduce().js
@@ -1,5 +1,6 @@
 // .reduce() = reduce elements of an array to a single value
 
+/*
 const prices = [5, 30, 10, 25, 15, 20];
 
 const total = prices.reduce(sum);
@@ -10,4 +11,21 @@ console.log(`$${total.toFixed(2)}`);
 // 35 + 10 = 45, etc etc
 function sum(previous, next) {
     return previous + next;
+}
+*/
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
+
+console.log(maximum);
+console.log(minimum);
+
+function getMax(previous, next) {
+    return Math.max(previous, next);
+}
+
+function getMin(previous, next) {
+    return Math.min(previous, next);
 }
