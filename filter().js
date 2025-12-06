4b9a3165f52cceda7f128bb793774eed9cebfd5c
william <espiw@farmingdale.edu>
2025-12-06 13:23:17 -0500

    Bro Code js filter() example two

--- filter().js
+++ filter().js
@@ -1,5 +1,6 @@
 // .filter() = creates a new array by filtering out elements
 
+/*
 let numbers = [1, 2, 3, 4, 5, 6, 7];
 let evenNums = numbers.filter(isEven);
 let oddNums = numbers.filter(isOdd);
@@ -13,4 +14,20 @@ function isEven(element) {
 
 function isOdd(element) {
     return element % 2 !== 0;
+}
+*/
+
+const ages = [16, 17, 18, 18, 19, 20, 60];
+let adults = ages.filter(isAdult);
+let children = ages.filter(isChild);
+
+console.log(adults);
+console.log(children);
+
+function isAdult(element) {
+    return element >= 18;
+}
+
+function isChild(element) {
+    return element < 18;
 }
