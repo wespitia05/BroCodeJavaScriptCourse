3320dae48f1b4d30bd766a6778567ff2cf9fae71
william <espiw@farmingdale.edu>
2025-12-06 13:20:48 -0500

    Bro Code js filter() example one

--- filter().js
+++ filter().js
@@ -1,1 +1,16 @@
 // .filter() = creates a new array by filtering out elements
+
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let evenNums = numbers.filter(isEven);
+let oddNums = numbers.filter(isOdd);
+
+console.log(evenNums);
+console.log(oddNums);
+
+function isEven(element) {
+    return element % 2 === 0;
+}
+
+function isOdd(element) {
+    return element % 2 !== 0;
+}
