d472a7270b4aaab5bf71a61059928d48345e5155
william <espiw@farmingdale.edu>
2025-12-07 00:36:16 -0500

    Bro Code js arrow functions setup

---
+++ arrow_functions.js
@@ -0,0 +1,3 @@
+// arrow functions = a concise way to write function expressions
+//                   good for simple functions that you use only once
+//                   (parameters) => some code
