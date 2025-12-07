582faeab5cf040bd7c4d73e836febae260400476
william <espiw@farmingdale.edu>
2025-12-07 00:24:38 -0500

    Bro Code js function expressions example two

--- function_expressions.js
+++ function_expressions.js
@@ -7,9 +7,34 @@ function hello() {
 */
 // function expressions = a way to define functions as 
 //                        values or variables
-
+/*
 const hello = function() {
     console.log("hello");
 }
 
 hello();
+*/
+
+// function declaration
+/*
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map(square);
+
+console.log(squares);
+
+function square(element) {
+    return Math.pow(element, 2);
+}
+*/
+
+// function expression
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map(function(element) {
+    return Math.pow(element, 2);
+})
+const cubes = numbers.map(function(element) {
+    return Math.pow(element, 3);
+})
+
+console.log(squares);
+console.log(cubes);
