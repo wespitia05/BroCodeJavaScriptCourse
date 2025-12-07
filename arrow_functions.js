c7332bfa95c5234681bd545d4dd0833566fa9e07
william <espiw@farmingdale.edu>
2025-12-07 13:15:46 -0500

    Bro Code js arrow functions example one

--- arrow_functions.js
+++ arrow_functions.js
@@ -1,3 +1,8 @@
 // arrow functions = a concise way to write function expressions
 //                   good for simple functions that you use only once
 //                   (parameters) => some code
+
+const hello = (name, age) => {console.log(`hello ${name}`);
+                         console.log(`you are ${age} years old`)};
+
+hello("william", 28);
