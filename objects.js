40513872a20db06917605324c44cd33c74309768
william <espiw@farmingdale.edu>
2025-12-07 13:24:46 -0500

    Bro Code js objects setup

---
+++ objects.js
@@ -0,0 +1,4 @@
+// object = a collection of related properties and/or methods
+//          can represent real world objects (people, products, places)
+//          object = {key:value,
+//                    function()}
