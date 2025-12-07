2c062200ed407bab152361d8a42a6d3f6c60d22c
william <espiw@farmingdale.edu>
2025-12-07 00:18:20 -0500

    Bro Code js functions expressions setup

---
+++ function_expressions.js
@@ -0,0 +1,5 @@
+// function declaration = define a reusable block of code
+//                        that performs a specific task
+
+// function expressions = a way to define functions as 
+//                        values or variables
