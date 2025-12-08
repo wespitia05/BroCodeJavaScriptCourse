2cf432a6f708019015cd254f24faaa08629af3d4
william <espiw@farmingdale.edu>
2025-12-08 17:30:58 -0500

    Bro Code js inheritance example

--- inheritance.js
+++ inheritance.js
@@ -1,3 +1,58 @@
 // inheritance = allows a new class to inherit properties and methods
 //               from an existing class (parent -> child)
 //               helps with code reusability
+
+class Animal {
+    alive = true;
+
+    eat() {
+        console.log(`this ${this.name} is eating`);
+    }
+
+    sleep() {
+        console.log(`this ${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal {
+    name = "rabbit";
+
+    run() {
+        console.log(`this ${this.name} is running`);
+    }
+}
+
+class Fish extends Animal {
+    name = "fish";
+
+    swim() {
+        console.log(`this ${this.name} is swimming`);
+    }
+}
+
+class Hawk extends Animal {
+    name = "hawk";
+
+    fly() {
+        console.log(`this ${this.name} is flying`);
+    }
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+rabbit.run();
+
+console.log(fish.alive);
+fish.eat();
+fish.sleep();
+fish.swim();
+
+console.log(hawk.alive);
+hawk.eat();
+hawk.sleep();
+hawk.fly();
