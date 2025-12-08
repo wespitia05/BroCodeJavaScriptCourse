8bd093640bc3c4ed452a60342ccdc97db80ee909
william <espiw@farmingdale.edu>
2025-12-08 13:34:27 -0500

    Bro Code js classes setup

---
+++ classes.js
@@ -0,0 +1,3 @@
+// class = (ES6 feature) provides a more structured and cleaner way to
+//         work with objects compared to traditional constructor functions
+//         ex. static keyword, encapsulation, inheritance
