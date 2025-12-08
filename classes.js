a534575205800c4586819e113fd9c6673c915266
william <espiw@farmingdale.edu>
2025-12-08 13:43:31 -0500

    Bro Code js classes example one continued

--- classes.js
+++ classes.js
@@ -12,7 +12,13 @@ class Product {
         console.log(`product: ${this.name}`);
         console.log(`price: ${this.price.toFixed(2)}`);
     }
+
+    calculateTotal(salesTax) {
+        return this.price + (this.price * salesTax);
+    }
 }
+
+const salesTax = 0.05;
 
 const product1 = new Product("shirt", 19.99);
 const product2 = new Product("pants", 22.50);
@@ -21,3 +27,11 @@ const product3 = new Product("underwear", 100.00);
 product1.displayProduct();
 product2.displayProduct();
 product3.displayProduct();
+
+const total1 = product1.calculateTotal(salesTax);
+const total2 = product2.calculateTotal(salesTax);
+const total3 = product3.calculateTotal(salesTax);
+
+console.log(`total price (with tax): $${total1.toFixed(2)}`);
+console.log(`total price (with tax): $${total2.toFixed(2)}`);
+console.log(`total price (with tax): $${total3.toFixed(2)}`);
