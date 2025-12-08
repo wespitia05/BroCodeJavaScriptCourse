010db0d3b67c0f1147234c25559d28c646066310
william <espiw@farmingdale.edu>
2025-12-08 13:48:55 -0500

    Bro Code js static keyword setup

---
+++ static_keyword.js
@@ -0,0 +1,3 @@
+// static = keyword that defines properties or methods that belong
+//          to a class itself rather than the objects created
+//          from that class (class owns anything static, not objects)
