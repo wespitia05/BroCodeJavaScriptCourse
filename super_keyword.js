9267bd2bed5240d02401170e12f942aa444cc395
william <espiw@farmingdale.edu>
2025-12-08 18:09:41 -0500

    Bro Code js super keyword setup

---
+++ super_keyword.js
@@ -0,0 +1,4 @@
+// super = keyword is used in classes to call the constructor or
+//         access the properties and methods of a parent (superclass)
+//         this = this object
+//         super = the parent
