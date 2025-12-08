83f9c384cf5a81c791a5b375448150e491d13c66
william <espiw@farmingdale.edu>
2025-12-08 14:01:57 -0500

    Bro Code js static keyword example two

--- static_keyword.js
+++ static_keyword.js
@@ -2,6 +2,7 @@
 //          to a class itself rather than the objects created
 //          from that class (class owns anything static, not objects)
 
+/*
 class MathUtil {
     static PI = 3.14159;
 
@@ -22,3 +23,34 @@ console.log(MathUtil.PI);
 console.log(MathUtil.getDiameter(10));
 console.log(MathUtil.getCircumference(10));
 console.log(MathUtil.getArea(10));
+*/
+
+class User {
+    static userCount = 0;
+
+    constructor(username) {
+        this.username = username;
+        User.userCount++;
+    }
+
+    sayHello() {
+        console.log(`hello my username is ${this.username}`);
+    }
+
+    static getUserCount() {
+        console.log(`there are ${User.userCount} users online`);
+    }
+}
+
+const user1 = new User("spongebob");
+const user2 = new User("patrick");
+const user3 = new User("sandy");
+
+console.log(user1.username);
+user1.sayHello();
+console.log(user2.username);
+user2.sayHello();
+console.log(user3.username);
+user2.sayHello();
+
+User.getUserCount();
