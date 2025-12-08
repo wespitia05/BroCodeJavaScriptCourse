772d05038386a7ca5597d61b707ff4a987900289
william <espiw@farmingdale.edu>
2025-12-08 13:55:02 -0500

    Bro Code js static keyword example one

--- static_keyword.js
+++ static_keyword.js
@@ -1,3 +1,24 @@
 // static = keyword that defines properties or methods that belong
 //          to a class itself rather than the objects created
 //          from that class (class owns anything static, not objects)
+
+class MathUtil {
+    static PI = 3.14159;
+
+    static getDiameter(radius) {
+        return radius * 2;
+    }
+
+    static getCircumference(radius) {
+        return 2 * this.PI * radius;
+    }
+
+    static getArea(radius) {
+        return this.PI * radius * radius;
+    }
+}
+
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
+console.log(MathUtil.getArea(10));
