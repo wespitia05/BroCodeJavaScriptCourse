fd624e533000fb8a8e37573b457b47dba2353d37
william <espiw@farmingdale.edu>
2025-12-09 21:42:09 -0500

    Bro Code js getters/setters example one

--- getters_setters.js
+++ getters_setters.js
@@ -2,3 +2,46 @@
 // setter = special method that makes a property writeable
 
 // validate and modify a value when reading/writing a property
+
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth) {
+        if(newWidth > 0) {
+            this._width = newWidth; // _width = private property
+        }
+        else {
+            console.error("width must be a positive number");
+        }
+    }
+
+    set height(newHeight) {
+        if(newHeight > 0) {
+            this._height = newHeight; // _height = private property
+        }
+        else {
+            console.error("height must be a positive number");
+        }
+    }
+
+    get width() {
+        return `${this._width.toFixed(1)}cm`;
+    }
+
+    get height() {
+        return `${this._height.toFixed(1)}cm`;
+    }
+
+    get area() {
+        return `${(this._width * this._height).toFixed(1)}cm^2`;
+    }
+}
+
+const rectangle = new Rectangle(3, 4);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
