3941497bc0e186e0b95b7ba8bd2072ff1650cbf0
william <espiw@farmingdale.edu>
2025-12-09 22:01:43 -0500

    Bro Code js destructuring

---
+++ destructuring.js
@@ -0,0 +1,5 @@
+// destructuring = extract vaues from arrays and objects
+//                 then assign them to variables in a convenient way
+//                 [] = to perform array destructuring
+//                 {} = to perform object destructuring
+//                 5 examples
