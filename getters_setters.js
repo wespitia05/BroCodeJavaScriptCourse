996a3e5d7b79eb6ab4357c17353b0e67a41d0d59
william <espiw@farmingdale.edu>
2025-12-09 21:33:55 -0500

    Bro Code js getters/setters setup

---
+++ getters_setters.js
@@ -0,0 +1,4 @@
+// getter = special method that makes a porperty readable
+// setter = special method that makes a property writeable
+
+// validate and modify a value when reading/writing a property
