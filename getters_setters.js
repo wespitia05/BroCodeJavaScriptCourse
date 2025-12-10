ff9afe3fbdc77ddb88260f6baec07058d5999e83
william <espiw@farmingdale.edu>
2025-12-09 21:53:32 -0500

    Bro Code js getters/setters example two

--- getters_setters.js
+++ getters_setters.js
@@ -3,6 +3,7 @@
 
 // validate and modify a value when reading/writing a property
 
+/*
 class Rectangle {
     constructor(width, height) {
         this.width = width;
@@ -45,3 +46,62 @@ const rectangle = new Rectangle(3, 4);
 console.log(rectangle.width);
 console.log(rectangle.height);
 console.log(rectangle.area);
+*/
+
+class Person {
+    constructor (firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName (newFirstName) {
+        if(typeof newFirstName === "string" && newFirstName.length > 0) {
+            this._firstName = newFirstName;
+        }
+        else {
+            console.error("first name must be a non-empty string");
+        }
+    }
+
+    set lastName (newLastName) {
+        if(typeof newLastName === "string" && newLastName.length > 0) {
+            this._lastName = newLastName;
+        }
+        else {
+            console.error("last name must be a non-empty string");
+        }
+    }
+
+    set age (newAge) {
+        if(typeof newAge === "number" && newAge >= 0) {
+            this._age = newAge;
+        }
+        else {
+            console.error("age must be a non-negative number");
+        }
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+    
+    get fullName() {
+        return this._firstName + " " + this._lastName;
+    }
+
+    get age() {
+        return this._age;
+    }
+}
+
+const person = new Person("spongebob", "squarepants", 30);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
