d8dc2a4b764109ab5555d451d45ec76fa3a1a46c
william <espiw@farmingdale.edu>
2025-12-10 21:57:31 -0500

    Bro Code js nested objects setup

---
+++ nested_objects.js
@@ -0,0 +1,6 @@
+// nested objects = objects inside of other Objects
+//                  allows you to represent more complex data structures
+//                  child objects is enclosed by a parents object
+
+//                  Person{Address{}, ContactInfo{}}
+//                  ShoppingVart{Keyboard{}, Mouse{}, Monitor{}}
