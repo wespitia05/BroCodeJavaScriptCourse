4cebce108a96ce16aa3fda0dc1d923a4dd9f3942
william <espiw@farmingdale.edu>
2025-12-11 23:27:12 -0500

    Bro Code js array of objects example two

--- array_of_objects.js
+++ array_of_objects.js
@@ -6,6 +6,7 @@ const fruits = [{name: "apple", color: "red", calories: 95},
                 {name: "coconut", color: "white", calories: 159}, 
                 {name: "pineapple", color: "yellow", calories: 37}];
 
+/*
 console.log(fruits[0].name);
 console.log(fruits[1].color);
 console.log(fruits[2].calories);
@@ -17,3 +18,25 @@ fruits.push({name: "grapes", color: "purple", calories: 62});
 // removes element at certain indeces
 // fruits.splice(1, 2);
 console.log(fruits);
+*/
+
+// ---------- forEach() ---------- //
+// displays calories only
+fruits.forEach(fruit => console.log(fruit.calories));
+// displays names only
+fruits.forEach(fruit => console.log(fruit.name));
+// displays color only
+fruits.forEach(fruit => console.log(fruit.color));
+
+// ---------- map() ---------- //
+// creates an array with just fruit names
+const fruitNames = fruits.map(fruit => fruit.name);
+console.log(fruitNames);
+
+// creates an array with just fruit colors
+const fruitColors = fruits.map(fruit => fruit.color);
+console.log(fruitColors);
+
+// creates an array with just fruit calories
+const fruitCalories = fruits.map(fruit => fruit.calories);
+console.log(fruitCalories);
