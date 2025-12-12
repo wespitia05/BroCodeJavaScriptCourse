f6921cc913d198b09d67f9e02a85550ce0189d2e
william <espiw@farmingdale.edu>
2025-12-11 23:21:21 -0500

    Bro Code js array of objects example one

--- array_of_objects.js
+++ array_of_objects.js
@@ -1,1 +1,19 @@
 // arrays of objects
+
+const fruits = [{name: "apple", color: "red", calories: 95}, 
+                {name: "orange", color: "orange", calories: 45}, 
+                {name: "banana", color: "yellow", calories: 105}, 
+                {name: "coconut", color: "white", calories: 159}, 
+                {name: "pineapple", color: "yellow", calories: 37}];
+
+console.log(fruits[0].name);
+console.log(fruits[1].color);
+console.log(fruits[2].calories);
+
+// adds an element
+fruits.push({name: "grapes", color: "purple", calories: 62});
+// removes an element
+// fruits.pop();
+// removes element at certain indeces
+// fruits.splice(1, 2);
+console.log(fruits);
