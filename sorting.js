0b5d7bdb3ac5928104f239c7f8362e7742859a67
william <espiw@farmingdale.edu>
2025-12-15 19:17:38 -0500

    Bro Code js sorting example two

--- sorting.js
+++ sorting.js
@@ -15,3 +15,23 @@ numbers.sort((a, b) => a - b);
 // numbers.sort((a, b) => b - a);
 
 console.log(numbers);
+
+const people = [{name: "spongebob", age: 30, gpa: 3.0},
+                {name: "patrick", age: 37, gpa: 1.5},
+                {name: "squidward", age: 51, gpa: 2.5},
+                {name: "sandy", age: 27, gpa: 4.0},];
+
+// sort by age
+// people.sort((a, b) => a.age - b.age);
+// reverse sort by age
+// people.sort((a, b) => b.age - a.age);
+// sort by gpa
+// people.sort((a, b) => a.gpa - b.gpa);
+// reverse sort by gpa
+// people.sort((a, b) => b.gpa - a.gpa);
+// sort by name
+people.sort((a, b) => a.name.localeCompare(b.name));
+// reverse sort by name
+// people.sort((a, b) => b.name.localeCompare(a.name));
+
+console.log(people);
