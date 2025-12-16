deefe399160d5e4c210bf2ddb8b4ee6fcd93f9dd
william <espiw@farmingdale.edu>
2025-12-15 19:12:33 -0500

    Bro Code js sorting example one

--- sorting.js
+++ sorting.js
@@ -1,3 +1,17 @@
 // sort() = method used to sort elements of an array in place
 //          sorts elements as string in lexicographic order, not alphabetical
 //          lexicographic = (alphabet + numbers + symbols) as strings
+
+let fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+
+fruits.sort();
+
+console.log(fruits);
+
+let numbers = [1, 10, 2, 9, 3, 8, 4, 7, 5, 6];
+
+numbers.sort((a, b) => a - b);
+// reverse sort
+// numbers.sort((a, b) => b - a);
+
+console.log(numbers);
