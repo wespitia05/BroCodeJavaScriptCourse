71f31a7c794dddb7341d6c70cfb050d50a04ef0b
william <espiw@farmingdale.edu>
2025-12-15 23:22:36 -0500

    Bro Code js closures setup

---
+++ closures.js
@@ -0,0 +1,5 @@
+// closure = a function defined inside of another function
+//           the inner function has access to the variables
+//           and scope of the outer function.
+//           allow for private variables and state maintenance
+//           used frequently in js frameworks: react, vue, angular
