cd869eca13264e8394ba1cea2f7f66ff4128bc13
william <espiw@farmingdale.edu>
2025-12-15 19:07:56 -0500

    Bro Code js sorting setup

---
+++ sorting.js
@@ -0,0 +1,3 @@
+// sort() = method used to sort elements of an array in place
+//          sorts elements as string in lexicographic order, not alphabetical
+//          lexicographic = (alphabet + numbers + symbols) as strings
