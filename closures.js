e9cb23fc425ff273da08d59924b7dc790adbcac0
william <espiw@farmingdale.edu>
2025-12-16 20:09:25 -0500

    Bro Code js closures example one

--- closures.js
+++ closures.js
@@ -3,3 +3,14 @@
 //           and scope of the outer function.
 //           allow for private variables and state maintenance
 //           used frequently in js frameworks: react, vue, angular
+
+function outer() {
+    // inner function has access to everything within outer function
+    let message = "hello"; // any variable considered private
+    function inner() {
+        console.log(message);
+    }
+    inner();
+}
+
+outer();
