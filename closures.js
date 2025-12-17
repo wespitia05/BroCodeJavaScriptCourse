bb9b5750b96882bfb8a900bf6893604356011b44
william <espiw@farmingdale.edu>
2025-12-16 20:41:07 -0500

    Bro Code js closures example three

--- closures.js
+++ closures.js
@@ -17,6 +17,7 @@ function outer() {
 outer();
 */
 
+/*
 function createCounter() {
     let count = 0;
 
@@ -40,3 +41,31 @@ counter.increment();
 counter.increment();
 
 console.log(`the current count is ${counter.getCount()}`);
+*/
+
+function createGame() {
+    let score = 0;
+
+    function increaseScore(points) {
+        score += points;
+        console.log(`+${points}pts`);
+    }
+
+    function decreaseScore(points) {
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+
+    function getScore() {
+        return score;
+    }
+
+    return {increaseScore, decreaseScore, getScore};
+}
+
+const game = createGame();
+
+game.increaseScore(5);
+game.increaseScore(6);
+game.decreaseScore(3);
+console.log(`the final score is ${game.getScore()}`);
