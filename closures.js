8a89171a980f9b3e14ae542dbe62dc36df1e5286
william <espiw@farmingdale.edu>
2025-12-16 20:30:47 -0500

    Bro Code js closures example two

--- closures.js
+++ closures.js
@@ -4,6 +4,7 @@
 //           allow for private variables and state maintenance
 //           used frequently in js frameworks: react, vue, angular
 
+/*
 function outer() {
     // inner function has access to everything within outer function
     let message = "hello"; // any variable considered private
@@ -14,3 +15,28 @@ function outer() {
 }
 
 outer();
+*/
+
+function createCounter() {
+    let count = 0;
+
+    function increment() {
+        count++;
+        console.log(`count incresed to ${count}`);
+    }
+
+    function getCount() {
+        return count;
+    }
+    return {increment, getCount};
+}
+
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(`the current count is ${counter.getCount()}`);
