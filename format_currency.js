d1899c864531d8d1754ffc49aa3544a91e810684
william <espiw@farmingdale.edu>
2025-12-19 22:54:40 -0500

    Bro Code js format currency setup

---
+++ format_currency.js
@@ -0,0 +1,9 @@
+// .toLocalString() = returns a string with a language
+//                    sensitive representation of a number
+
+// Intl.NumberFormat()
+
+// number.toLocaleString("locale", {options});
+
+// 'locale' = specify the language (undefined = default set in browser)
+// 'options' = object with formatting options
