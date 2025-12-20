8264919a768d7e1395e7012bee16bf5617a1b809
william <espiw@farmingdale.edu>
2025-12-19 22:22:31 -0500

    Bro Code js setTimeout() setup

---
+++ setTimeout.js
@@ -0,0 +1,5 @@
+// setTimeout() = function in javascript that allows you to schedule
+//                the execution of a function after an amount of time (ms)
+//                Times are approximate (varies based on the workload of the javascript runtime env.)
+
+//                setTimeout(callback, delay);
