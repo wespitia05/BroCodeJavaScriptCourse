5b50087d490a7fc3a921fccc55df8d79486f92fd
william <espiw@farmingdale.edu>
2025-12-19 23:01:03 -0500

    Bro Code js format currency examples

--- format_currency.js
+++ format_currency.js
@@ -7,3 +7,16 @@
 
 // 'locale' = specify the language (undefined = default set in browser)
 // 'options' = object with formatting options
+
+let number = 123456.789;
+
+// number = number.toLocaleString("en-US"); // usa
+// number = number.toLocaleString("hi-IN"); // india
+// number = number.toLocaleString("de-DE"); // germany
+// number = number.toLocaleString(undefined); // usa(default)
+
+// number = number.toLocaleString("en-US", {style: "currency", currency: "USD"}); // usa currency format
+// number = number.toLocaleString("hi-IN", {style: "currency", currency: "INR"}); // india currency format
+number = number.toLocaleString("de-DE", {style: "currency", currency: "EUR"}); // germany currency format
+
+console.log(number);
