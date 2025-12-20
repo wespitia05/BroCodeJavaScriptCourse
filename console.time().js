37b51ea24bd70eeed97561ff16855d2ce5fa8ef4
william <espiw@farmingdale.edu>
2025-12-19 22:33:26 -0500

    Bro Code js console.time() setup

---
+++ console.time().js
@@ -0,0 +1,6 @@
+// console.time() = tool that allows you to measure the time it takes
+//                  for a section of code or process to execute
+//                  great for identifying performance "bottlenecks"
+
+// console.time("label");
+// console.timeEnd("label");
