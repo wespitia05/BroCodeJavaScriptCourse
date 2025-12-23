749664fd29ab2f6f479dd3fc15909554df91ba9a
william <espiw@farmingdale.edu>
2025-12-23 14:51:19 -0500

    Bro Code js asynchronous example

--- asynchronous.js
+++ asynchronous.js
@@ -5,3 +5,19 @@
 //                doesn't block the execution flow and allows the program to continue
 //                (I/O operations, network requests, fetching data)
 //                handled with: callbacks, promises, async/await
+
+// using callback will have the code wait for the first task to run and then let the other tasks run
+function func1() {
+    // asynchronous. runs while the other code is running
+    setTimeout(() => {console.log("task 1");
+                      cancelIdleCallback()}, 3000);
+}
+
+function func2() {
+    // synchronous
+    console.log("task 2");
+    console.log("task 3");
+    console.log("task 4");
+}
+
+func1(func2);
