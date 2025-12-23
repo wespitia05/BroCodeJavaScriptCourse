7280ceaab147f5c9878aa0ab1c4e9ec07a6750a6
william <espiw@farmingdale.edu>
2025-12-23 15:45:37 -0500

    Bro Code js error handling example two

--- error_handling.js
+++ error_handling.js
@@ -6,6 +6,7 @@
 // finally { } = (optional) always executes. used mostly for clean up
 //               ex. close files, close connections, release resources
 
+/*
 try {
     console.log(x);
     // NETWORK ERRORS
@@ -23,3 +24,25 @@ finally {
 }
 
 console.log("you reached the end");
+*/
+
+try {
+    const dividend = Number(window.prompt("enter a dividend: "));
+    const divisor = Number(window.prompt("enter a divisor: "));
+
+    if(divisor == 0) {
+        throw new Error("you cant divide by zero");
+    }
+    if(isNaN(dividend) || isNaN(divisor)) {
+        throw new Error("values must be a number");
+    }
+
+    const result = dividend / divisor;
+
+    console.log(result);    
+}
+catch(error) {
+    console.error(error);
+}
+
+console.log("you have reached the end");
