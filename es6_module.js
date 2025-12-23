c0246c075413d6b1319b305fd35f702ddfdd94ab
william <espiw@farmingdale.edu>
2025-12-23 12:26:55 -0500

    Bro Code js es6 modules setup

---
+++ es6_module.js
@@ -0,0 +1,5 @@
+// ES6 Module = an external file that contains reusable code
+//              that can be imported into other javascript files.
+//              write reusable code for many different apps.
+//              can contain variables, classes, functions ... and more
+//              introduced as part of ECMAScript 2015 update
