b23c83fd603db5d5c4206c3df55a256883305dab
william <espiw@farmingdale.edu>
2025-12-23 15:26:01 -0500

    Bro Code js error handling example one

--- error_handling.js
+++ error_handling.js
@@ -1,2 +1,25 @@
 // error = an object that is created to represent a problem that occurs
 //         occurs often with user input or establishing a connection
+
+// try { } = encloses code that might potentially cause an error
+// catch { } = catch and handle any thrown errors from try { }
+// finally { } = (optional) always executes. used mostly for clean up
+//               ex. close files, close connections, release resources
+
+try {
+    console.log(x);
+    // NETWORK ERRORS
+    // PROMISE REJECTION
+    // SECURITY ERRORS
+}
+catch (error) {
+    console.error(error);
+}
+finally {
+    // CLOSE FILES
+    // CLOSE CONNECTIONS
+    // RELEASE RESOURCES
+    console.log("this always executes");
+}
+
+console.log("you reached the end");
