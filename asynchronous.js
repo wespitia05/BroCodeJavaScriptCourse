a592c1b02217868d8b72ada50d936cf26bbc2fe9
william <espiw@farmingdale.edu>
2025-12-23 14:21:26 -0500

    Bro Code js asynchronous setup

---
+++ asynchronous.js
@@ -0,0 +1,7 @@
+// synchronous = executes line by line consecutively in a sequential manner
+//               code that waits for an operation to complete
+
+// asynchronous = allows multiple operations to be performed concurrently without waiting
+//                doesn't block the execution flow and allows the program to continue
+//                (I/O operations, network requests, fetching data)
+//                handled with: callbacks, promises, async/await
