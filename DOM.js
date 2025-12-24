23e58c60d9d64afd40eb92232f68770cdf7dfbb2
william <espiw@farmingdale.edu>
2025-12-24 18:02:36 -0500

    Bro Code js DOM setup

---
+++ DOM.js
@@ -0,0 +1,7 @@
+// DOM = DOCUMENT OBJECT MODEL
+//       Object() that represents the page you see in the web browser
+//       and provides you with an API to interact with it.
+//       web browser constructs the DOM when it loads and HTML document,
+//       and structures all the elements in a tree-like representation
+//       javascript can access the DOM to dynamically
+//       change the content, structure and style of a web page
