b33f596b390e1d79b90c3f05e1eae73ef054f15c
william <espiw@farmingdale.edu>
2025-12-26 16:18:04 -0500

    Bro Code js element selectors setup

---
+++ element_selectors.js
@@ -0,0 +1,9 @@
+// element selectors = methods used to target and manipulate html elements
+//                     they allow you to select one or multiple html elements
+//                     from the DOM (document object model)
+
+// 1. document.getElementById()       // element or null
+// 2. document.getElementsClassName() // html collection
+// 3. document.getElementsByTagName() // html collection
+// 4. document.querySelector()        // element or null
+// 5. document.querySelectorAll()     // nodelist
