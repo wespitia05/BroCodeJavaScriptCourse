4067f0c679df7c828eb6bac3b9ee863e3fa8ef58
william <espiw@farmingdale.edu>
2025-12-26 21:02:43 -0500

    Bro Code js element selectors getElementsClassName() example

--- element_selectors.js
+++ element_selectors.js
@@ -8,9 +8,29 @@
 // 4. document.querySelector()        // element or null
 // 5. document.querySelectorAll()     // nodelist
 
+/*
 //selecting h1 element by its ID
 const myHeading = document.getElementById("my-heading");
 myHeading.style.backgroundColor = "yellow";
 myHeading.style.textAlign = "center";
 
 console.log(myHeading); // displays html element + style
+*/
+
+// selecting class fruits by its class name
+const fruits = document.getElementsByClassName("fruits");
+
+// highlights selected fruit
+// fruits[0].style.backgroundColor = "yellow";
+
+// enhanced for loop to highlight all fruits
+/* for(let fruit of fruits) {
+    fruit.style.backgroundColor = "yellow";
+} */
+
+// another method to highlight all fruits
+Array.from(fruits).forEach(fruit => {
+    fruit.style.backgroundColor = "yellow";
+});
+
+console.log(fruits); // displays html collection of fruits along with its properties
