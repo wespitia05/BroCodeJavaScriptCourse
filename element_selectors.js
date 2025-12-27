f1bb7bb652241c339bbe9f6c743dfb287cb10c2a
william <espiw@farmingdale.edu>
2025-12-26 21:10:51 -0500

    Bro Code js element selectors getElementByTagName() example

--- element_selectors.js
+++ element_selectors.js
@@ -18,7 +18,7 @@ console.log(myHeading); // displays html element + style
 */
 
 // selecting class fruits by its class name
-const fruits = document.getElementsByClassName("fruits");
+// const fruits = document.getElementsByClassName("fruits");
 
 // highlights selected fruit
 // fruits[0].style.backgroundColor = "yellow";
@@ -29,8 +29,37 @@ const fruits = document.getElementsByClassName("fruits");
 } */
 
 // another method to highlight all fruits
-Array.from(fruits).forEach(fruit => {
+/* Array.from(fruits).forEach(fruit => {
     fruit.style.backgroundColor = "yellow";
-});
+}); */
 
-console.log(fruits); // displays html collection of fruits along with its properties
+// console.log(fruits); // displays html collection of fruits along with its properties
+
+// selecting tag name h4 by its tag name
+const h4Elements = document.getElementsByTagName("h4");
+const liElements = document.getElementsByTagName("li");
+
+// highlights specified h4 element yellow
+// h4Elements[0].style.backgroundColor = "yellow";
+
+// highlights all h4 elements yellow using enhanced for loop
+for(let h4Element of h4Elements) {
+    h4Element.style.backgroundColor = "yellow";
+}
+
+// highlights all li elements light green using enhanced for loops
+for(let liElement of liElements) {
+    liElement.style.backgroundColor = "lightgreen";
+}
+
+// typecasting h4 elements as an array to highlight yellow
+Array.from(h4Elements).forEach(h4Element => {
+    h4Element.style.backgroundColor = "yellow";
+})
+
+// typecasting li elements as an array to highlight lightgreen
+Array.from(liElements).forEach(liElement => {
+    liElement.style.backgroundColor = "lightgreen";
+})
+
+console.log(h4Elements); // displays html collection and its properties
