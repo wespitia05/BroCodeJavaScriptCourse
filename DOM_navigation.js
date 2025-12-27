e419dedfd297f874c4ceb7458106512a3f0160d8
william <espiw@farmingdale.edu>
2025-12-26 22:42:39 -0500

    Bro Code js DOM navigation setup

---
+++ DOM_navigation.js
@@ -0,0 +1,9 @@
+// DOM navigation = the process of navigating through the structure
+//                  of an html document using javascript
+
+// .firstElementChild
+// .lastElementChild
+// .nextElementChild
+// .previousElementSibling
+// .parentElement
+// .children
