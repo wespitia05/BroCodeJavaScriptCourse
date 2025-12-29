a3e41df367d61a361ecfe54d320d7f227e24fed7
william <espiw@farmingdale.edu>
2025-12-29 12:26:38 -0500

    Bro Code js DOM navigation .lastElementChild example

--- DOM_navigation.js
+++ DOM_navigation.js
@@ -9,6 +9,7 @@
 // .children
 
 // ---------- .firstElementChild ---------- //
+/*
 // highlights the first element of the fruits unordered list
 const element = document.getElementById("fruits");
 const firstChild = element.firstElementChild;
@@ -19,4 +20,18 @@ const ulElements = document.querySelectorAll("ul");
 ulElements.forEach(ulElement => {
     const firstChild = ulElement.firstElementChild;
     firstChild.style.backgroundColor = "yellow";
+})
+*/
+
+// ---------- .lastElementChild ---------- //
+// highlights the last element of the fruits unordered list
+const element = document.getElementById("fruits");
+const lastChild = element.lastElementChild;
+lastChild.style.backgroundColor = "yellow";
+
+// highlights the last element of all unordered lists
+const ulElements = document.querySelectorAll("ul");
+ulElements.forEach(ulElement => {
+    const lastChild = ulElement.lastElementChild;
+    lastChild.style.backgroundColor = "yellow";
 })
