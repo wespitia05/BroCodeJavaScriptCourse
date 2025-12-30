fd9d6dffee49b771690a013975d38032f50f05e2
william <espiw@farmingdale.edu>
2025-12-30 13:46:53 -0500

    Bro Code js mouse events click example

--- mouse_events.js
+++ mouse_events.js
@@ -1,3 +1,12 @@
 // eventListener = listen for specific events to create interactive web pages
 //                 events: click, mouseover, mouseout
 //                 .addEventListener(event, callback)
+
+const myBox = document.getElementById("myBox");
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "tomato";
+    event.target.textContent = "OUCH! 🤕";
+}
+
+myBox.addEventListener("click", changeColor);
