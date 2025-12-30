148c1e249c99ca0aa077ffc9292b3aca5e3fc0bb
william <espiw@farmingdale.edu>
2025-12-30 14:52:00 -0500

    Bro Code js key events setup

---
+++ key_events.js
@@ -0,0 +1,3 @@
+// eventListener = listen for specific events to create interactive web pages
+//                 events: keydown, keyup
+//                 document.addEventListener(event, callback)
