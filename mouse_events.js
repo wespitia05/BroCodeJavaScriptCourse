a60aedec37ce85a7a00522057710cfcce52a2052
william <espiw@farmingdale.edu>
2025-12-30 13:37:21 -0500

    Bro Code js mouse events setup

---
+++ mouse_events.js
@@ -0,0 +1,3 @@
+// eventListener = listen for specific events to create interactive web pages
+//                 events: click, mouseover, mouseout
+//                 .addEventListener(event, callback)
