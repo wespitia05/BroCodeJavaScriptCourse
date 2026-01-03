5fb6089275576948289d3cb6de4eaa866416f1bf
william <espiw@farmingdale.edu>
2026-01-03 13:24:38 -0500

    Bro Code js key events quick example with just javascript

--- key_events.js
+++ key_events.js
@@ -1,3 +1,11 @@
 // eventListener = listen for specific events to create interactive web pages
 //                 events: keydown, keyup
 //                 document.addEventListener(event, callback)
+
+document.addEventListener("keydown", event => {
+    console.log(`key down = ${event.key}`);
+})
+
+document.addEventListener("keyup", event => {
+    console.log(`key up = ${event.key}`);
+})
