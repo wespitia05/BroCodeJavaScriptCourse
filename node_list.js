4de50b4a6a15f375d3d8c15678d21dc9e05f23af
william <espiw@farmingdale.edu>
2026-01-03 13:53:42 -0500

    Bro Code js node list setup

---
+++ node_list.js
@@ -0,0 +1,4 @@
+// nodelist = static collection of html elements by (id, class, element)
+//            can be created by using querySelectorAll()
+//            similar to an array, but no (map, filter, reduce)
+//            nodelist wont update to automatically reflact changes
