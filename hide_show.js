9fa6f0121804da0e9c34e3c8e2f59973e75900a1
william <espiw@farmingdale.edu>
2026-01-03 13:47:02 -0500

    Bro Code js hide/show html functionality

--- hide_show.js
+++ hide_show.js
@@ -1,1 +1,15 @@
 // hide and show html functionality
+
+const myButton = document.getElementById("myButton");
+const myImg = document.getElementById("myImg");
+
+myButton.addEventListener("click", event => {
+    if(myImg.style.visibility === "hidden") {
+        myImg.style.visibility = "visible";
+        myButton.textContent = "Hide";
+    }
+    else {
+        myImg.style.visibility = "hidden";
+        myButton.textContent = "Show";
+    }
+})
