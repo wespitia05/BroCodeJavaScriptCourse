e4fa1dfa00390135ef3bc101c6eb2f366e73b38d
william <espiw@farmingdale.edu>
2026-01-03 13:36:57 -0500

    Bro Code js key events moving html element with key events

--- key_events.js
+++ key_events.js
@@ -3,6 +3,9 @@
 //                 document.addEventListener(event, callback)
 
 const myBox = document.getElementById("myBox");
+const moveAmount = 10;
+let x = 0; // horizontal movement
+let y = 0; // vertical movement
 
 document.addEventListener("keydown", event => {
     myBox.textContent = "😵";
@@ -12,4 +15,28 @@ document.addEventListener("keydown", event => {
 document.addEventListener("keyup", event => {
     myBox.textContent = "😄";
     myBox.style.backgroundColor = "lightblue";
+})
+
+document.addEventListener("keydown", event => {
+    console.log(event.key);
+    event.preventDefault();
+    // if the key starts with "Arrow", move forward
+    if (event.key.startsWith("Arrow")) {
+        switch(event.key) {
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case  "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;
+        }
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
 })
