683b0106b8993efa64231bfc99041502d7cf1938
william <espiw@farmingdale.edu>
2026-01-04 18:11:09 -0500

    Bro Code js node lists adding html/css properties

--- node_list.js
+++ node_list.js
@@ -2,3 +2,11 @@
 //            can be created by using querySelectorAll()
 //            similar to an array, but no (map, filter, reduce)
 //            nodelist wont update to automatically reflact changes
+
+let buttons = document.querySelectorAll(".myButtons");
+
+// add hml/css properties
+buttons.forEach(button => {
+    button.style.backgroundColor = "green";
+    button.textContent += " 😄";
+})
