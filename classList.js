7364103327b64446afef8c9716449e3cc13dc1b3
william <espiw@farmingdale.edu>
2026-01-04 18:25:16 -0500

    Bro Code js class list setup

---
+++ classList.js
@@ -0,0 +1,10 @@
+// classList = element property in javascript used to interact
+//             with an element's list of classes (css classes)
+//             allows you to make reusable classes for many elements
+//             across your webpage
+
+// add()
+// remove()
+// toggle(remove if present, add if not)
+// replace(oldClass, newClass)
+// contains()
