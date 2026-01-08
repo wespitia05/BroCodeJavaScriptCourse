674194dd64b50dd936973642e77628c89cc180e1
william <espiw@farmingdale.edu>
2026-01-08 18:35:57 -0500

    Bro Code js json using stringify

--- json.js
+++ json.js
@@ -4,3 +4,44 @@
 
 //        JSON.stringify() = converts a JS object to a JSON string
 //        JSON.parse() = converts a JSON string to a JS object
+
+const names = ["Spongebob", "Patrick", "squidward", "Sandy"];
+const person = {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["jellyfishing", "karate", "cooking"]
+};
+const people =
+[{
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true
+},
+{
+    "name": "Patrick",
+    "age": 34,
+    "isEmployed": false
+},
+{
+    "name": "Squidward",
+    "age": 50,
+    "isEmployed": true
+},
+{
+    "name": "Sandy",
+    "age": 27,
+    "isEmployed": false
+}]
+
+// converts json objects into a string
+const jsonNames = JSON.stringify(names);
+const jsonPerson = JSON.stringify(person);
+const jsonPeople = JSON.stringify(people);
+
+console.log(names);
+console.log(jsonNames);
+console.log(person);
+console.log(jsonPerson);
+console.log(people);
+console.log(jsonPeople);
