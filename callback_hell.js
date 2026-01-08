6de8f6fda62db91d1fb9a6c9621a5fe2107477e0
william <espiw@farmingdale.edu>
2026-01-07 19:33:44 -0500

    Bro Code js callback hell example

--- callback_hell.js
+++ callback_hell.js
@@ -3,3 +3,39 @@
 //                 where the code is difficult to read.
 //                 old pattern to handle asynchronous functions.
 //                 use promises + async/await to avoid callback hell
+
+function task1(callback) {
+    setTimeout(() => {
+        console.log("task 1 complete");
+        callback();        
+    }, 2000);
+}
+
+function task2(callback) {
+    setTimeout(() => {
+        console.log("task 2 complete");
+        callback();         
+    }, 1000);
+}
+
+function task3(callback) {
+    setTimeout(() => {
+        console.log("task 3 complete");
+        callback();         
+    }, 3000);
+}
+
+function task4(callback) {
+    setTimeout(() => {
+        console.log("task 4 complete");
+        callback();         
+    }, 1500);
+}
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => console.log("all tasks compelete"));
+        });
+    });
+});
