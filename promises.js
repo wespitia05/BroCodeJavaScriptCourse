d91f4da17cbf4d695d5b8a3bc4d1c68868494721
william <espiw@farmingdale.edu>
2026-01-07 19:36:37 -0500

    Bro Code js promises setup

---
+++ promises.js
@@ -0,0 +1,11 @@
+// promise = an objects that manages asynchronous operations
+//           wrap a promise object around {asynchronous code}
+//           "i promise to return a value"
+//           PENDING -> RESOLVED or REJECTED
+//           new Promise((resolve, reject) => {asynchronous code})
+
+// DO THESE CHORES IN ORDER
+
+// 1. WALK THE DOG
+// 2 CLEAN THE KITCHEN
+// 3. TAKE OUT THE TRASH
