ba8a6ee9b37f3df38e75cf1dec536f2920c6dd86
william <espiw@farmingdale.edu>
2026-01-07 19:57:44 -0500

    Bro Code js promises example

--- promises.js
+++ promises.js
@@ -9,3 +9,50 @@
 // 1. WALK THE DOG
 // 2 CLEAN THE KITCHEN
 // 3. TAKE OUT THE TRASH
+
+function walkDog() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dogWalked = true;
+            if(dogWalked) {
+                resolve("you walk the dog 🦮");
+            }
+            else{
+                reject("you didn't walk the dog");
+            }
+        }, 1500);
+    });
+}
+
+function cleanKitchen() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const kitchenCleaned = true;
+            if(kitchenCleaned) {
+                resolve("you clean the kitchen 🧹");
+            }
+            else{
+                reject("you didn't clean the kitchen");
+            }
+        }, 1500);
+    });
+}
+
+function takeOutTrash() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const trashTakenOut = false;
+            if(trashTakenOut) {
+                resolve("you take out the trash 🗑️");
+            }
+            else{
+                reject("you didn't take out the trash");
+            }
+        }, 500);
+    });
+}
+
+walkDog().then(value => {console.log(value); return cleanKitchen()})
+         .then(value => {console.log(value); return takeOutTrash()})
+         .then(value => {console.log(value); console.log("you finished all chores")})
+         .catch(error => console.error(error));
