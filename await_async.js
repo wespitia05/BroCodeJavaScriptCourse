e489630f127cc7da5e4fafa067b01d5c3a601cb0
william <espiw@farmingdale.edu>
2026-01-07 20:10:26 -0500

    Bro Code js await/async setup

---
+++ await_async.js
@@ -0,0 +1,6 @@
+// async/await = async = makes a function return a promise
+//               await = makes an async function wait for a promise
+
+//               allows you to write asynchronous code in a synchronous manner
+//               async doesn't have resolve or reject parameters
+//               everything after await is placed in an event queue
