594878abf3476b293ff8fd553363a2fea0cf259b
william <espiw@farmingdale.edu>
2026-01-07 19:27:55 -0500

    Bro Code js callback hell setup

---
+++ callback_hell.js
@@ -0,0 +1,5 @@
+// callback hell = situation in javascript where callbacks are
+//                 nested within other callback to the degree
+//                 where the code is difficult to read.
+//                 old pattern to handle asynchronous functions.
+//                 use promises + async/await to avoid callback hell
