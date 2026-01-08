7ae679ff4194e2e752db027cac2d8bf8558ecc2d
william <espiw@farmingdale.edu>
2026-01-07 21:04:10 -0500

    Bro Code js json setup

---
+++ json.js
@@ -0,0 +1,6 @@
+// JSON = (javascript object notation) data-interchange format
+//        used for exchanging data between a server and a web application
+//        JSON files {key:value} OR [value1, value2, value3]
+
+//        JSON.stringify() = converts a JS object to a JSON string
+//        JSON.parse() = converts a JSON string to a JS object
