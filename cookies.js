f620e3b704dbc84c0cc48084f078236f8cb5a230
william <espiw@farmingdale.edu>
2026-01-09 18:04:11 -0500

    Bro Code js cookies creating/enabling

--- cookies.js
+++ cookies.js
@@ -1,3 +1,13 @@
 // cookie = a small text file stored on your computer
 //          used to remember information about the user
 //          saved in name=value pairs
+
+// check to see if you have cookies enabled
+console.log(navigator.cookieEnabled);
+
+// create our own cookie
+document.cookie = "firstName=Spongebob; expires=Sun, 1 January 2030 12:00:00 EST; path=/";
+document.cookie = "lastName=Squarepants; expires=Sun, 1 January 2030 12:00:00 EST; path=/";
+
+// printing out the cookie
+console.log(document.cookie);
