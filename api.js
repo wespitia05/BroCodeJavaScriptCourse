0f043d110cb580e31bac716510767c190db41f13
william <espiw@farmingdale.edu>
2026-01-10 16:06:11 -0500

    Bro Code js api getting data using fetch

--- api.js
+++ api.js
@@ -4,3 +4,13 @@
 //         used for interacting with api's to retrieve and send
 //         data asynchronously over the web
 //         fetch(url, {options})
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+        if(!response.ok) {
+            throw new Error("coul not fetch resource");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data.name))
+    .catch(error => console.error(error));
