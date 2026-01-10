78b0c8a910607dded4d89805cfb8bb949a563ca8
william <espiw@farmingdale.edu>
2026-01-10 13:07:18 -0500

    Bro Code js cookies function to create a cookie

--- cookies.js
+++ cookies.js
@@ -2,6 +2,7 @@
 //          used to remember information about the user
 //          saved in name=value pairs
 
+/*
 // check to see if you have cookies enabled
 console.log(navigator.cookieEnabled);
 
@@ -11,3 +12,15 @@ document.cookie = "lastName=Squarepants; expires=Sun, 1 January 2030 12:00:00 ES
 
 // printing out the cookie
 console.log(document.cookie);
+*/
+
+setCookie("email", "Sponge@gmail.com", 365);
+
+console.log(document.cookie);
+
+function setCookie(name, value, daysToLive) {
+    const date = new Date();
+    date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
