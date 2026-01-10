6ee440a1a92a26005db8f4b06740508bd4940d39
william <espiw@farmingdale.edu>
2026-01-10 13:17:41 -0500

    Bro Code js API setup

---
+++ api.js
@@ -0,0 +1,6 @@
+// fetch = function used for making http requests to fetch resources
+//         (json style data, images, files)
+//         simplifies asynchronous data fetching in javascript and
+//         used for interacting with api's to retrieve and send
+//         data asynchronously over the web
+//         fetch(url, {options})
