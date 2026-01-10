8e6aeb821b87a70cfd886bbf020f0e363684dece
william <espiw@farmingdale.edu>
2026-01-10 16:19:16 -0500

    Bro Code js api fetching and displaying pokemon image

--- api.js
+++ api.js
@@ -19,14 +19,19 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
 // using async()
 async function fetchData() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/typhlosion")
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         
         if(!response.ok) {
             throw new Error("could not fetch resouce");
         }
 
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
     }
     catch(error) {
         console.error(error);
